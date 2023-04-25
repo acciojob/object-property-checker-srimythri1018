1b239a9eccba4cc07d71f06dc90b28cb9717f499
srimythri1018 <115878123+srimythri1018@users.noreply.github.com>
2023-04-25 22:15:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-	for(let i=0; i< sampleObject.length ;i++){
-  if(key == sampleObject[i]){
-	  return true;
-  }
-	else {
-		return false;
-	}
-	}//   write your code here
+	return key in sampleObject;//   write your code here
 }
 
 // Do not change the code below
